c664fcdf380860ec9b21b7697688831d692e96a6
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-14 23:14:09 +0530

    Time: 3 ms (59.41%), Space: 7.6 MB (95.93%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -4,7 +4,7 @@ public:
         vector<string> str;
         int     t=0;
         for(int i=1;t<target.size(); i++)
-        {  cout<<"ji";
+        { 
             if(target[t]==i) {str.push_back("Push");t++;}
             else {str.push_back("Push");str.push_back("Pop");}
         }
