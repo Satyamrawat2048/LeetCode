a541dab3a11ddc73fb7c47af4e0e6cd9970ca198
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-15 23:07:46 +0530

    Time: 71 ms (68.33%), Space: 13.8 MB (7.68%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int countQuadruplets(vector<int>& nums) {
-        int n= nums.size(),c=0;
-        for(int i=0 ;i<n;i++)
+        int count = 0;
+        int n = nums.size();
+        unordered_map<int, int> freq;
+
+        freq[nums[n-1]]=1;
+        
+        for(int i=n-2 ; i> 1 ; i--)
         {
-            for(int j =i+1 ; j<n ; j++)
+            for(int j=i-1 ; j>0; j--)
             {
-                for(int k=j+1 ; k<n ; k++)
-                {
-                    for(int l= k+1 ; l<n  ; l++)
-                    {
-                        if(nums[i] + nums[j] + nums[k] == nums[l])c++;
-                    }
-                }
+                for(int k =j-1 ; k>=0 ; k--)
+                    count+= freq[nums[i]+nums[j]+nums[k]];
             }
+         freq[nums[i]]+= 1;
         }
-   return c; }
+        return count;
+    }
 };
