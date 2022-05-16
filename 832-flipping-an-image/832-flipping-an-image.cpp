dfb6c0fa6d9943d671dbf626303913ed833cacaf
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-16 20:14:46 +0530

    Time: 4 ms (82.86%), Space: 8.6 MB (99.83%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.cpp
+++ 832-flipping-an-image/832-flipping-an-image.cpp
@@ -1,15 +1,36 @@
+// class Solution {
+// public:
+//     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        
+//         for(int i= 0 ; i< image.size();i++)
+//         {
+//          reverse(image[i].begin(),image[i].end());
+//          for(int j= 0 ; j<image[i].size();j++) 
+//          { 
+//              if(image[i][j]==0) image[i][j]=1 ;
+//              else image[i][j]=0 ;
+//          }
+//         }
+//         return image ;
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
         
         for(int i= 0 ; i< image.size();i++)
-        {
-         reverse(image[i].begin(),image[i].end());
-         for(int j= 0 ; j<image[i].size();j++) 
-         { 
-             if(image[i][j]==0) image[i][j]=1 ;
-             else image[i][j]=0 ;
-         }
+        { int s=0, e= image[i].size()-1 ;
+         
+           while(s<=e)
+           {
+               if(image[i][s]==image[i][e])
+               {
+                  image[i][s]=1-image[i][s];
+                   image[i][e]= image[i][s];
+               }
+               s++ ;e--;
+           }
         }
         return image ;
     }
