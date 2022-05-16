833faa7f8ad8d49c9b3424882617954b9190e92b
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-16 19:42:15 +0530

    Time: 47 ms (20.18%), Space: 13.8 MB (67.62%) - LeetHub

--- 929-unique-email-addresses/929-unique-email-addresses.cpp
+++ 929-unique-email-addresses/929-unique-email-addresses.cpp
@@ -1,35 +1,56 @@
+// class Solution {
+// public:
+    
+//     int numUniqueEmails(vector<string>& nums) {
+//         int n= nums.size() ;
+//         map<string,int> mp ;
+        
+//         for(int i=0; i< n ; i++)
+//         { string s="";
+//          int j=0;
+//             while(nums[i][j]!='@')
+//             {  if(nums[i][j] =='+'){break;}
+             
+//                else if(nums[i][j]=='.'){ j++; continue ;}
+             
+//                 s+=nums[i][j];j++ ;
+//             }
+//           if(nums[i][j]=='+') 
+//               while(nums[i][j]!='@') j++;
+          
+//          while(j<nums[i].size())
+//              { s+=nums[i][j];j++;}
+         
+//          nums[i]=s ;
+//          mp[s]++;
+//         }
+//         int c=0 ;
+//         for(auto it: mp)
+//         { cout<<it.first<<" ";
+//             c++ ;
+//         }
+//         return c;
+//     }
+// };
+
 class Solution {
 public:
-    
     int numUniqueEmails(vector<string>& nums) {
         int n= nums.size() ;
-        map<string,int> mp ;
+        set<string> str ;
         
         for(int i=0; i< n ; i++)
         { string s="";
-         int j=0;
-            while(nums[i][j]!='@')
-            {  if(nums[i][j] =='+'){break;}
-             
-               else if(nums[i][j]=='.'){ j++; continue ;}
-             
-                s+=nums[i][j];j++ ;
+            for(char j: nums[i])
+            {  
+               if(j =='+'||j =='@' )break ;
+               else if(j=='.') continue ;
+               s+=j;
             }
-          if(nums[i][j]=='+') 
-              while(nums[i][j]!='@') j++;
           
-         while(j<nums[i].size())
-             { s+=nums[i][j];j++;}
-         
-         
-         nums[i]=s ;
-         mp[s]++;
+         s+= nums[i].substr(nums[i].find('@'));
+         str.insert(s);
         }
-        int c=0 ;
-        for(auto it: mp)
-        { cout<<it.first<<" ";
-            c++ ;
-        }
-        return c;
+       return str.size();
     }
 };
