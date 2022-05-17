7557efd388355d8f74665d7752873c820bafdc04
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-17 19:51:55 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (63.95%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -8,22 +8,37 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    int getDecimalValue(ListNode* head) {
-        int sum =0, c=0;
-        
-        ListNode* temp= head ;
-        
-        while(temp)
+// class Solution {
+// public:
+//     int getDecimalValue(ListNode* head) 
+//     {
+//         int sum =0, c=0;
+//         ListNode* temp= head ;
+//         while(temp)
+//         {
+//             temp=temp->next ; c++;
+//         }
+//         c--;
+//         while(head)
+//         {  sum+= head->val *(pow(2,c)) ;
+//             c--; head= head->next ;
+//         }
+//         return sum;
+//     }
+// };
+
+class Solution
+{
+    public:
+    int getDecimalValue(ListNode* head)
+    {
+        int sum=1; 
+        if(head->val==0)  sum=0;
+        while(head->next!= NULL)
         {
-            temp=temp->next ; c++;
+            sum=sum*2 + head->next->val ;
+            head= head->next ;
         }
-        c--;
-        while(head)
-        {  sum+= head->val *(pow(2,c)) ;
-            c--; head= head->next ;
-        }
-        return sum;
+        return sum ;
     }
 };
