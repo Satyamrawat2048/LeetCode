9ce3ba3fdf33dc48fa6e738c0831cb9789a95201
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-17 23:47:38 +0530

    Time: 7 ms (27.52%), Space: 6.7 MB (68.87%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-    char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());  sort(t.begin(),t.end());
-        int i=0;
-        for( i= 0 ; i< t.size()&& i< s.size();i++)
-        {
-            if(t[i]!=s[i])return t[i] ;
+    // char findTheDifference(string s, string t) { O(nlogn)
+    //     sort(s.begin(),s.end());  sort(t.begin(),t.end());
+    //     int i=0;
+    //     for( i= 0 ; i< t.size()&& i< s.size();i++)
+    //     {
+    //         if(t[i]!=s[i])return t[i] ;
+    //     }
+    //     return t[i];
+    // }
+    
+        char findTheDifference(string s, string t) //O(n)
+        { 
+            char c=0 ;
+            
+            for(int i=0 ; i< s.size(); i++)c^= s[i];
+            for(int i=0 ; i< t.size(); i++) c^=t[i];
+            return c;            
         }
-        return t[i];
-
-        return 1;
-    }
 };
