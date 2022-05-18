f36648ca87a66de3601d2e62b2973d26471cc92b
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-18 13:18:27 +0530

    Time: 4 ms (40.57%), Space: 7.8 MB (73.13%) - LeetHub

--- 722-remove-comments/722-remove-comments.cpp
+++ 722-remove-comments/722-remove-comments.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
-    vector<string> removeComments(vector<string>& s) {
-        vector<string> ans; 
-        string str ="";
-        int n = s.size();
-        bool comment= false;
-        for(int i=0 ; i<n ; i++)
-        { 
-            for(int j=0 ; j<s[i].size() ; j++)
-            { if(!comment && j<s[i].size()&& s[i][j]=='/' && s[i][j+1]=='/') break;
+//     vector<string> removeComments(vector<string>& s) {
+//         vector<string> ans; 
+//         string str ="";
+//         int n = s.size();
+//         bool comment= false;
+//         for(int i=0 ; i<n ; i++)
+//         { 
+//             for(int j=0 ; j<s[i].size() ; j++)
+//             { if(!comment && j<s[i].size()&& s[i][j]=='/' && s[i][j+1]=='/') break;
               
-             else if(!comment && j<s[i].size()&& s[i][j]=='/' && s[i][j+1]=='*') comment=true, j++;
+//              else if(!comment && j<s[i].size()&& s[i][j]=='/' && s[i][j+1]=='*') comment=true, j++;
              
-             else if(comment && j<s[i].size() && s[i][j]=='*' && s[i][j+1]=='/') comment = false ,j++ ;
+//              else if(comment && j<s[i].size() && s[i][j]=='*' && s[i][j+1]=='/') comment = false ,j++ ;
              
-             else if(!comment) str.push_back(s[i][j]) ;
+//              else if(!comment) str.push_back(s[i][j]) ;
+//             }
+//          if(!comment && str.size()) 
+//          ans.push_back(str), str.clear();
+//         }
+//         return ans ;
+//     }
+    
+        vector<string> removeComments(vector<string>& str) {
+            vector<string > ans ; string s ;
+            bool comment =false ;
+            
+            for(string l : str)
+            {
+                for( int i=0 ; i< l.size(); i++)
+                {
+                    string two = l.substr(i,2);
+                    if(!comment)
+                    {   if(two=="//") break;
+                        else if(two=="/*") comment= true, i++;
+                       else s.push_back(l[i]) ;
+                    }
+                    else if(two=="*/") comment= false, i++;
+                }
+         if(!comment && s.size()) ans.push_back(s) , s.clear();
             }
-         if(!comment && str.size()) 
-         ans.push_back(str), str.clear();
+            return ans ;
         }
-        return ans ;
-    }
+
     
 };
