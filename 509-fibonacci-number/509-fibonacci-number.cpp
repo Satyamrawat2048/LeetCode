255dfd7d86a6644f941edc41228a055e8780137e
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-20 19:08:21 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.99%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -4,14 +4,25 @@ public:
        // if(n<2) return n ; O(2^n)
        //  return fib(n-1) + fib(n-2) ;
         
-        //O(n) using memoization
+        // TC and SC O(n) using memoization
+        // if(n<2) return n;
+        // int m[n+1] ;
+        // m[0]=0;
+        // m[1]=1; 
+        // for(int i=2;  i<= n ; i++)
+        //     m[i]=m[i-1]+m[i-2];
+        // return m[n];    
+        
+        //Bottom up dp  TC O(n) SC O(1)
         
         if(n<2) return n;
-        int m[n+1] ;
-        m[0]=0;
-        m[1]=1; 
-        for(int i=2;  i<= n ; i++)
-            m[i]=m[i-1]+m[i-2];
-        return m[n];    
+        
+        int a=0, b=1, c=0 ;
+        for(int i=1; i< n;i++)
+        {
+            c=a+b ; 
+            a=b; b=c ;
+        }
+        return c ;
     }
 };
