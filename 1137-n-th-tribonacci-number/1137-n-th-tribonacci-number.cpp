0909a05ea4f1f2491b2d3f5fd52aa48cb566932e
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-20 19:40:35 +0530

    Time: 2 ms (36.54%), Space: 5.9 MB (51.24%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    long arr[38]={0};
+public:
+    
+    int tribonacci(int n) {
+      if(n<2) return n;
+        if(n==2) return 1;
+        if(arr[n]!=0) return arr[n];
+        return arr[n] =tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+    }
+};
