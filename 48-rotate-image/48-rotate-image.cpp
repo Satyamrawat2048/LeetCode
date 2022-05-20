93ad85b5b47d43742fb7a7ae71321e8bc94e9ae6
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-20 23:09:39 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (34.16%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+// Rotate using another matrix
 //     void rotate(vector<vector<int>>& matrix) {
 //         int n = matrix.size();
 //        vector<vector<int>> ans(n,vector<int> (n,0));
@@ -20,15 +21,24 @@ public:
 //                }
 //        }
 //     }
-        void rotate(vector<vector<int>>& a) 
-        {
-            int n= a.size();
+    
+    //Rotate by firstly transposing matrix then reversing each row 
+//         void rotate(vector<vector<int>>& a) 
+//         {
+//             int n= a.size();
             
-            for(int i= 0;i<n; i++ )
-                for(int j=i+1 ; j<n ; j++)
-                 swap(a[i][j], a[j][i]);
+//             for(int i= 0;i<n; i++ )
+//                 for(int j=i+1 ; j<n ; j++)
+//                  swap(a[i][j], a[j][i]);
             
-            for(int i=0 ; i< n ; i++)
-            reverse(a[i].begin(),a[i].end()) ;
-        }
+//             for(int i=0 ; i< n ; i++)
+//             reverse(a[i].begin(),a[i].end()) ;
+//         }
+    void rotate(vector<vector<int> > &matrix) {
+    reverse(matrix.begin(), matrix.end());//reverse columns first 
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+}
 };
