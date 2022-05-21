30eab5f9c271b8d963133a506a9ecf871a069958
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-21 16:26:55 +0530

    Time: 7 ms (75.30%), Space: 13.7 MB (50.57%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -12,18 +12,34 @@ public:
     //     return dp[n];
     
     //bottom up dp with O(1) SC
-    int minCostClimbingStairs(vector<int>& cost) {
-    int n= cost.size();
-    int a=0, b=0 , c=0 ;
+//     int minCostClimbingStairs(vector<int>& cost) {
+//     int n= cost.size();
+//     int a=0, b=0 , c=0 ;
         
-    for(int i=2  ; i<=n; i++)
-    {
-        int jumpone= b+cost[i-1];
-        int jumptwo= c+ cost[i-2];
-        a= min(jumpone, jumptwo );
-        c=b ;
-        b=a ;
+//     for(int i=2  ; i<=n; i++)
+//     {
+//         int jumpone= b+cost[i-1];
+//         int jumptwo= c+ cost[i-2];
+//         a= min(jumpone, jumptwo );
+//         c=b ;
+//         b=a ;
+//     }
+//         return b;
+//     }
+    //Memoization o(n)
+    vector<int> memo ;
+    int minCostClimbingStairs(vector<int>& cost) {
+        // memoization + recn topvto down  as we are going from n to 0
+        int n = cost.size();
+        memo.resize(n,0);
+        return min(calcost(cost,n-1), calcost(cost,n-2));
     }
-        return b;
+    int calcost(vector<int> &cost,int n)
+    {
+        if(n<0) return 0;
+        if(n==1|| n==0) return  cost[n];
+        if(memo[n]!=0) return memo[n];
+        memo[n]=cost[n]+ min(calcost(cost,n-1),calcost(cost,n-2));
+        return memo[n];
     }
 };
