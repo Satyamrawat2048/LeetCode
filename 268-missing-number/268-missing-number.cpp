b1c03052d07908f7b259043aa38f508b9d2d0b7e
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-21 17:54:27 +0530

    Time: 19 ms (83.79%), Space: 18.1 MB (21.08%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        //using sort O(nlogn)
         //using map o(n)
 //      int n= nums.size() ;
 //         map<int,int> mp ;
@@ -11,14 +12,20 @@ public:
 //         if(mp[i]==0)return i;
 //         return 0;
         
-        //using maths o(n)
+    //using maths o(n)
+//      int n= nums.size() ;
+//         int sum1= n*(n+1)/2 ,sum=0;
+//         for(int i=1 ; i<n ; i++)
+//         sum+= nums[i];
         
-     // using map o(n)
-     int n= nums.size() ;
-        int sum1= n*(n+1)/2 ,sum=0;
-        for(int i=0 ; i<n ; i++)
-        sum+= nums[i];
-        
-        return sum1-sum;
-    }
+//         return sum1-sum;
+
+// bit manipulation
+      int res= nums.size();
+        for(int i=0 ; i< nums.size(); i++)
+        {
+            res^=i;
+            res^=nums[i];
+        }
+    return res; }
 };
