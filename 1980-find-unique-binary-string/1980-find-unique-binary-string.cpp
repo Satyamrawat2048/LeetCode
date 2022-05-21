77d53b951992cbe0cc34a95fb348fc9ade31c908
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-21 23:28:08 +0530

    Time: 12 ms (20.75%), Space: 10.2 MB (61.27%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,29 +1,41 @@
+// class Solution {
+// public:
+//     void combination(int n, int ind, set<string>&st, string s, vector<string>&perm)
+//     {
+//         if(ind==n)
+//         {
+//             if(st.find(s)==st.end())
+//                 perm.push_back(s);
+//         return ;}
+//         combination(n, ind+1, st, s+"0", perm);
+//         if(perm.size()!=0) return ;
+        
+//         combination(n, ind+1, st, s+"1", perm);
+//         if(perm.size()!=0)return ;
+        
+//     }
+//     string findDifferentBinaryString(vector<string>& nums) {
+        
+//         set<string>st;
+//         for(int i=0;i<nums.size();i++)
+//             st.insert(nums[i]);
+        
+//         vector<string>perm;
+//         string s ="";
+//         int n = nums[0].size();
+//         combination(n,0,st,s,perm);
+//         return perm[0];
+//     }
+// };
 class Solution {
 public:
-    void combination(int n, int ind, set<string>&st, string s, vector<string>&perm)
-    {
-        if(ind==n)
-        {
-            if(st.find(s)==st.end())
-                perm.push_back(s);
-        return ;}
-        combination(n, ind+1, st, s+"0", perm);
-        if(perm.size()!=0) return ;
-        
-        combination(n, ind+1, st, s+"1", perm);
-        if(perm.size()!=0)return ;
-        
-    }
     string findDifferentBinaryString(vector<string>& nums) {
         
-        set<string>st;
-        for(int i=0;i<nums.size();i++)
-            st.insert(nums[i]);
+        string res;
         
-        vector<string>perm;
-        string s ="";
-        int n = nums[0].size();
-        combination(n,0,st,s,perm);
-        return perm[0];
+        for (int i = 0; i < nums.size(); i++) 
+            res += nums[i][i] == '0' ? "1" : "0"; 
+        
+        return res; 
     }
 };
