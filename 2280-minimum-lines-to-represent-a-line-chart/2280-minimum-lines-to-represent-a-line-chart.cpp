c77159c2efad932aaad45a2c6a7b518ca77c8bf9
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-22 13:18:17 +0530

    Time: 563 ms (66.67%), Space: 100.6 MB (33.33%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-//     float roundoff(float item, int c)
-// {
-//   float p10 = pow(10.0f, c);
-//   return round(item * p10) /p10;
-// }
-    
     int minimumLines(vector<vector<int>>& sp) {
-        int n = sp.size(),c=1;
-
-                if(n<=1) return 0;
-
-        sort(sp.begin(),sp.end());
-        for(int i=1 ; i<n -1; i++)
-        {
-            long double x1= sp[i-1][0];
-            long double y1= sp[i-1][1];
+        sort(sp.begin(), sp.end());
+        if (sp.size() <= 1) return 0;
             
-       long double x2= sp[i][0];
-       long double y2= sp[i][1];
+        long double prev = (sp[1][1] - sp[0][1]) / (long double)(sp[1][0] - sp[0][0]);
+        int lines = 1;
+        for (int i = 2; i < sp.size(); i++) {
+            long double slope = (sp[i][1] - sp[i-1][1]) / (long double)(sp[i][0] - sp[i-1][0] );
             
-        long double x3= sp[i+1][0];
-       long double y3= sp[i+1][1];
-
-            long double a= (long double)(y2-y1)/(x2-x1 ) ;
-            long double b= (long double)(y3-y2)/(x3-x2 ) ;
-
-            if(a!=b)c++ ;
+            if (slope != prev) {
+                lines++;
+                prev = slope;
+            }
         }
-       return c;
+        return lines;
+    
     }
 };
