34c4ae87c469d939cd76444fc4dcc649eddb63bb
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-22 12:19:54 +0530

    Time: 850 ms (33.33%), Space: 118 MB (33.33%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maximumBags(vector<int>& cap, vector<int>& rock, int a ) 
+    {int n=cap.size();
+        vector<int> rem(n,0) ;
+        
+        for(int i=0; i< n ; i++)
+            rem[i]=cap[i]-rock[i];
+        
+         map<int,int> mp ;
+
+        for(int i=0 ; i<n; i++)
+        { mp[i]=i ; //mp[i] has ind
+        }
+     sort(rem.begin(),rem.end()) ;
+     
+      int k=0;
+     for(int i=0 ; i< n ; i++)
+           {   if(rem[i]==0) k++;
+              else if( rem[i]<=a) {a-= rem[i] ;k++;}
+              
+            if(a==0) break ;
+           }
+     for(int i: rem)
+        cout<<i<<endl;
+     
+           // for(int i=0 ; i< n ; i++)
+           // {
+           //     if(i==0)k++ ; 
+           // }
+     return k;
+    }
+};
