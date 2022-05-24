a473501bfc3ec05f5ecccb9d858b021eb321e7e6
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-25 01:52:31 +0530

    Time: 13 ms (35.31%), Space: 14.9 MB (15.01%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,30 +11,43 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode*l3=new ListNode() ;
+//         ListNode*l3=new ListNode() ;
         
-        if(!l1) return l2;
-        if(!l2) return l1;
+//         if(!l1) return l2;
+//         if(!l2) return l1;
         
-      if(l1->val <l2->val) {l3= l1 ;
-                             l1=l1->next ;}
-        else {l3=l2 ; 
-              l2=l2->next ;}
-     ListNode*node=l3;
-        while(l1 && l2)
-        {
-            if(l1->val <l2->val)
-            { l3->next=l1 ;
-                l1=l1->next ;
-            }
-            else {l3->next=l2 ;
-                l2=l2->next ;
-               }
-            l3=l3->next ;
-        }
-       if(l1) l3->next=l1 ;
-        else l3->next =l2 ;
+//       if(l1->val <l2->val) {l3= l1 ;
+//                              l1=l1->next ;}
+//         else {l3=l2 ; 
+//               l2=l2->next ;}
+//      ListNode*node=l3;
+//         while(l1 && l2)
+//         {
+//             if(l1->val <l2->val)
+//             { l3->next=l1 ;
+//                 l1=l1->next ;
+//             }
+//             else {l3->next=l2 ;
+//                 l2=l2->next ;
+//                }
+//             l3=l3->next ;
+//         }
+//        if(l1) l3->next=l1 ;
+//         else l3->next =l2 ;
        
-        return node;
+//         return node;
+        //recursive soln
+        if(!l1) return l2;
+        if(!l2) return l1; 
+        if(l1->val <l2->val)
+        {
+            l1->next=mergeTwoLists(l1->next,l2);
+            return l1;
+        }
+        else
+        {
+            l2->next=mergeTwoLists(l1,l2->next);
+            return l2;
+        }
     }
 };
