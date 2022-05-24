17a8e74be46b63c0f046e61eb287a0cc660230f7
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-24 23:53:43 +0530

    Time: 7 ms (73.45%), Space: 8.3 MB (42.65%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -11,25 +11,35 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-     vector<int> v; 
-        if(head==NULL) return NULL ;
-        while(head)
-        {
-            v.push_back(head->val);
-            head=head->next ;
+     // vector<int> v; //using additional space
+     //    if(head==NULL) return NULL ;
+     //    while(head)
+     //    {
+     //        v.push_back(head->val);
+     //        head=head->next ;
+     //    }
+     //    ListNode*temp=new ListNode()  ;
+     //        temp->val=v[v.size()-1] ;
+     //        temp->next=NULL ;
+     //    head=temp ;
+     //    for(int i=v.size()-2;i>-1;i--)
+     //    {
+     //        ListNode*n1=new ListNode()  ;
+     //        n1->val=v[i] ;
+     //        n1->next=NULL ;
+     //        temp->next=n1 ;
+     //        temp=temp->next ;
+     //    }
+     //    return head;
+        
+        //using 3 ptrs
+        ListNode*prev = NULL;
+        while(head != NULL){
+            ListNode*nxt = head->next;
+            head->next = prev;
+            prev = head;
+            head = nxt;
         }
-        ListNode*temp=new ListNode()  ;
-            temp->val=v[v.size()-1] ;
-            temp->next=NULL ;
-        head=temp ;
-        for(int i=v.size()-2;i>-1;i--)
-        {
-            ListNode*n1=new ListNode()  ;
-            n1->val=v[i] ;
-            n1->next=NULL ;
-            temp->next=n1 ;
-            temp=temp->next ;
-        }
-        return head;
+        return prev;
     }
 };
