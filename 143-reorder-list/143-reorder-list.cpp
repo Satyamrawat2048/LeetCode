76367da44933ecc30de5cc9f8dd3d4b883798038
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-26 15:26:04 +0530

    Time: 87 ms (14.12%), Space: 17.8 MB (52.07%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -10,30 +10,88 @@
  */
 class Solution {
 public:
+    
     void reorderList(ListNode* head) {
-        if(!head || !(head->next) || !(head->next->next))
-            return ;
+        //uisng stack
+//         if(!head || !(head->next) || !(head->next->next))
+//             return ;
         
-        stack<ListNode*> st ;
-        ListNode*p=head ;
-        int s=0 ;
+//         stack<ListNode*> st ;
+//         ListNode*p=head ;
+//         int s=0 ;
         
-        while(p)
-        { s++ ;
-         st.push(p);
-         p=p->next ;
-        }
+//         while(p)
+//         { s++ ;
+//          st.push(p);
+//          p=p->next ;
+//         }
         
-        ListNode*p2=head ;
+//         ListNode*p2=head ;
         
-        for(int i=0 ; i<s/2 ;i++)
+//         for(int i=0 ; i<s/2 ;i++)
+//         {
+//             ListNode*temp =st.top();
+//             st.pop();
+//             temp->next=p2->next ;
+//             p2->next = temp ;
+//             p2= p2->next->next ;
+//         }
+//         p2->next=NULL ;
+        
+        //using recursion
+//         head=rec(head);        
+//     }
+//     ListNode* rec(ListNode*head )
+//     {
+//         if(!(head->next) || !(head->next->next))
+//             return head ;
+        
+//         ListNode*p =head ;
+//         while(p->next->next)
+//             p=p->next ;
+        
+//         p->next->next= head->next ;
+//         head->next = p->next ;
+//         p->next =NULL ;
+//         head->next->next = rec(head->next->next);
+//         return head ;
+        
+        //using 2 ptr
+       ListNode * mid=findmid(head) ,*midnext=mid->next;
+        mid->next=NULL ;
+        
+        ListNode*p2= reverse(midnext);
+        ListNode*p1=head ,*p1next ;
+        
+         while(p1 && p2){
+            p1next =  p1->next;                                    
+			p1->next =  p2;
+			           
+            p1 =  p2;
+            p2=p1next;  
+         }
+        
+    }
+    ListNode* findmid(ListNode*head)
+    {
+        ListNode*fast=head  , *slow =head ;
+        while(fast && fast ->next)
         {
-            ListNode*temp =st.top();
-            st.pop();
-            temp->next=p2->next ;
-            p2->next = temp ;
-            p2= p2->next->next ;
+            fast=fast->next->next;
+            slow=slow->next ;
         }
-        p2->next=NULL ;
+    return slow ;
+    }
+    ListNode* reverse(ListNode*head)
+    {
+        ListNode*prev=NULL ,*nxt=NULL ;
+        while(head)
+        {
+            nxt=head->next ;
+            head->next=prev;
+            prev=head ;
+            head=nxt ;
+        }
+        return prev ;
     }
 };
