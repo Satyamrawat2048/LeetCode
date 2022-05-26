3fc63210fce98c890156d364f20c13d11b248b33
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-26 23:04:01 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.42%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -4,7 +4,6 @@ public:
        if(num<=0 ) return false ;
        for(int i=1 ;i< num ;i+=2)
        {
-            if(num<0)return false;
         for(int i=0 ;i<32;i+=2)
             if(num==1<<i)return true;
         return false;
