60328c8b119f3f5a74e3d89d4a0ee9a494a20227
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-05-27 21:40:27 +0530

    Time: 1184 ms (21.27%), Space: 256.3 MB (58.45%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -10,21 +10,18 @@
  */
 class Solution {
 public:ListNode* mergeNodes(ListNode* head){
-    //BASE CASE -> if we have a single zero, simply return null
-    if(!head->next) return nullptr;
+
+    if(!(head->next)) return NULL ;
     
-    //fetch sum from current 0 to next 0
-    ListNode* ptr= head->next;
-    int sum=0;
-    while(ptr->val!=0) sum+= ptr->val, ptr=ptr->next;
+    ListNode* p =head->next ;
+    int sum=0 ;
+    while(p->val) 
+        sum+=p->val ,p=p->next  ;
     
-    //assign sum on the first node between nodes having value 0.
-    head->next->val= sum;
-    
-    //call and get the answer and connect the answer to next of head->next
-    head->next->next= mergeNodes(ptr);
-    
-    //return head->next..=> new head
-    return head->next;
+        //assign sum on the first node between nodes having value 0.
+
+    head->next->val = sum;
+    head->next->next = mergeNodes(p);
+    return head->next ;
 }
 };
