56475e73739c19d7ed66d0f6c00300660d5a79a1
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-06-06 19:52:27 +0530

    Time: 12 ms (61.19%), Space: 12 MB (26.74%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
@@ -4,7 +4,9 @@ public:
         int c=0; 
         
         for(int i=0 ; i<words.size();i++)
+        { if(words[i].size()> s.size())continue ;
            if(s.substr(0,words[i].size())== words[i]) c++;
+        }
     return c;
     }
 };
