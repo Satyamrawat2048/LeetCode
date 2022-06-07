10813ac47a01b24af8967b8ebd72add5bf7b8829
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-06-07 09:50:47 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     void merge(vector<int>& a, int m, vector<int>& b, int n) {
-    vector<int> v(m+n);
-        int i=0 , j=0 , k=0;
+//     vector<int> v(m+n); notinplace
+//         int i=0 , j=0 , k=0;
         
-        while(i<m && j< n)
-        {
-            if(a[i]> b[j]) v[k]= b[j] , k++, j++ ;
-            else v[k]= a[i] , k++, i++ ;
+//         while(i<m && j< n)
+//         {
+//             if(a[i]> b[j]) v[k]= b[j] , k++, j++ ;
+//             else v[k]= a[i] , k++, i++ ;
+//         }
+//         while(i<m)
+//        v[k]= a[i] , k++, i++ ;
+        
+//         while(j<n) 
+//         v[k]= b[j] , k++, j++ ;
+        
+//         a= v ;   
+        
+        //inplace
+        int i=m-1 , j=n-1 , k=m+n-1 ;
+        
+        while( j>=0 )
+        {  if(i>=0 &&a[i] > b[j])  a[k]=a[i], k-- ,i--;
+           else  a[k]= b[j] , k--, j--;
         }
-        while(i<m)
-       v[k]= a[i] , k++, i++ ;
-        
-        while(j<n) 
-        v[k]= b[j] , k++, j++ ;
-        
-        a= v ;        
     }
 };
