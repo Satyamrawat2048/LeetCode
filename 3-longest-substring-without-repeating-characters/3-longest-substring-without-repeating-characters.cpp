af28206f6cee4d66db81e21c301d1327ec571127
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-06-11 17:54:29 +0530

    Time: 64 ms (18.53%), Space: 10.2 MB (33.68%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,49 +1,32 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // int c= 0, maxi =0 ; if only alphabets were present wrong soln
-        // vector<int> v(26, 0); 
-        // for(int i=0 ; i<s.size();i++)
-        // {   if(v[s[i]-'a']<1 ) {v[s[i]-'a']++ ;c++;
-        //                        }
-        //    else 
-        //    {   maxi= max(maxi,c);
-        //        v.clear();
-        //        v[s[i]-'a']++ ;
-        //        c=1;
-        //    }
-        // }
-        // return max(maxi ,c);
         
-//         vector<int> freq(256, -1);
+        if(s.length()==0)return 0;  
+        map<char,int> m;   
         
-//         int maxLen = 0, start = -1;
-//         for (int i = 0; i < s.length(); i++) {
-//             if (freq[s[i]] > start)
-//                 start = freq[s[i]];
-            
-//             freq[s[i]] = i;
-//             maxLen = max(maxLen, i - start);
-//         }
-//         return maxLen;
+        int i=0,j=0,ans=INT_MIN;
         
-         if( s == "") { return 0; }
-        
-        map<char, int> freq;       
-        
-        int longest = 1;
-        int j = 0;
-        
-        for (int i = 0; i < s.length(); ++i)
+        while(j<s.length())   
         {
-            if ( freq.find(s[i]) != freq.end() )
+            m[s[j]]++;  
+            if(m.size()==j-i+1)  // whem map size is equal to the window size means suppose window size is 3 and map size is also three that means in map all unique characters are their
             {
-                j = j >= freq.find(s[i])->second + 1 ? j : freq.find(s[i])->second + 1;
+                ans = max(ans,j-i+1);  //compare the length of the maximum window size
             }
-            freq[s[i]] = i;
-            longest = longest >= i - j + 1 ? longest : i - j + 1;
+            else if(m.size()<j-i+1)   //if the map size is less than the window size means there is some duplicate present like window size = 3 and map size = 2 means there is a duplicates
+            {
+                while(m.size()<j-i+1)  //so till the duplicates are removed completely
+                {
+                    m[s[i]]--;   //remove the duplicates
+                    if(m[s[i]]==0)  //if the frequency becomes zero 
+                        m.erase(s[i]);//delete it completely
+                    
+                    i++;  //go for next element 
+                }
+            }
+             j++;  //go for the next element
         }
-        
-        return longest;
+        return ans;
     }
 };
