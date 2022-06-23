49155544689734cffe4b2c098a6b7fcfb59a8c3b
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-06-23 23:36:04 +0530

    Time: 360 ms (51.31%), Space: 63.5 MB (29.84%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int scheduleCourse(vector<vector<int>>& courses) {
         if(courses.size() <= 0) return 0;
-        sort(courses.begin(), courses.end(), [](const vector<int>& a, vector<int>& b) {
+        sort(courses.begin(), courses.end(), []( vector<int>& a, vector<int>& b) {
             return a[1] < b[1];
         });
         priority_queue<int> q;
