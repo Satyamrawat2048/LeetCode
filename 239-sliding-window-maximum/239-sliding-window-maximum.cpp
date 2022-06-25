ee332d659c0a6342c7d61601de5f9e70fdf4d9e3
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-06-25 18:48:11 +0530

    Time: 354 ms (76.79%), Space: 134.7 MB (50.12%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
-
-     vector<int> maxSlidingWindow(vector<int>&v, int k) 
-    {    int n= v.size();
-      vector<int> ans;
-     queue<int> q;
-     multiset<int> p ;
+         vector<int> maxSlidingWindow(vector<int>&v, int k) {
+//     {    int n= v.size();
+//       vector<int> ans;
+//      queue<int> q;
+//      multiset<int> p ;
      
-     for(int i=0 ; i<k; i++)
-     {
-         q.push(v[i]) ; p.insert(v[i]) ;
-     }
-     ans.push_back(*p.rbegin()) ;
-     p.erase(p.find(q.front())) ; q.pop() ;
+//      for(int i=0 ; i<k; i++)
+//      {
+//          q.push(v[i]) ; p.insert(v[i]) ;
+//      }
+//      ans.push_back(*p.rbegin()) ;
+//      p.erase(p.find(q.front())) ; q.pop() ;
    
-        for(int i=k ; i<n; i++ )
-        {  
-          q.push(v[i])  ; p.insert(v[i]) ;
+//         for(int i=k ; i<n; i++ )
+//         {  
+//           q.push(v[i])  ; p.insert(v[i]) ;
             
-         ans.push_back(*(p.rbegin())); 
-         p.erase(p.find(q.front())) ;  q.pop() ;
-        }
-        return ans;
+//          ans.push_back(*(p.rbegin())); 
+//          p.erase(p.find(q.front())) ;  q.pop() ;
+//         }
+//         return ans;
+  int n= v.size();
+    vector<int> result;
+    deque<int> w;
+    
+    for (int i = 0;  i < n; i++) {
+        
+        while (!w.empty() && w.front() <= i-k)
+            w.pop_front();
+        
+        while (!w.empty() && v[w.back()] <= v[i])
+            w.pop_back();
+        
+        w.push_back(i);
+        
+        if (i >= k-1)
+            result.push_back(v[w.front()]);
+    }
+    return result;
      }
 };
