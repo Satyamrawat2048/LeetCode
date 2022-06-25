2f16e16778919b3a399de477109d29edd15a2411
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-06-25 11:36:39 +0530

    Time: 42 ms (46.41%), Space: 27.1 MB (53.59%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool checkPossibility(vector<int>& nums) {
+    bool checkPossibility(vector<int>& nums) 
+    {
         int c=0,i ;
         int n= nums.size();
         if(n==1 ||n==2) return 1;
@@ -8,7 +9,6 @@ public:
         { 
             if(nums[i]>nums[i+1])
             { if(i>0 && i<n-2 && nums[i]>nums[i+2] && nums[i-1]> nums[i+1] ) return 0;
-             // if(nums[i-1]> nums[i+1] && num[i+2]< nums[i])
               c++;
             }
             if(c>1) return 0;
