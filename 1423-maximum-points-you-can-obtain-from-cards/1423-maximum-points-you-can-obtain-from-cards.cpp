d3a7f3f4e8822f61610207cbbc735800d758d34b
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-06-27 23:43:04 +0530

    Time: 118 ms (18.81%), Space: 42.3 MB (80.70%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,27 +1,27 @@
-class Solution {
-public:
-   int maxScore(vector<int>& card, int k) {
-        int n = card.size();
-        int ans=0 ;
-        vector<int> pre(n+1,0),suf(n+1,0) ;
-        for(int i=1; i<n+1 ; i++)
-            pre[i]= pre[i-1]+ card[i-1] ;
+//    int maxScore(vector<int>& card, int k) {
+//         int n = card.size(),ans=0 ;
+//         vector<int> pre(n+1,0),suf(n+1,0) ;
+//         for(int i=1; i<n+1 ; i++)            pre[i]= pre[i-1]+ card[i-1] ;
 
-        for(int i=n-1; i>=0 ; i--)
-            suf[i]= suf[i+1]+ card[i] ;
+//         for(int i=n-1; i>=0 ; i--)  suf[i]= suf[i+1]+ card[i] ;
 
-        reverse(suf.begin(),suf.end()) ;
+//         reverse(suf.begin(),suf.end()) ;
 
-         ans =0; 
+//         for(int i=0 ; i<= k; i++)
+//             ans = max(ans, pre[i]+suf[k-i]);
        
-        for(int i=0 ; i<= k; i++)
-        { //cout<<pre[i]<<' '<< suf[k-i]<<endl ;
-            ans = max(ans, pre[i]+suf[k-i]);}
-        // for(int i:suf)
-        //     cout<<i<<' ';
-        //     cout<<endl;
-        //  for(int i:pre)
-        //     cout<<i<<' ';
-        return ans; 
+//         return ans; 
+//     }
+
+class Solution
+{ // see sgallivan soln for hint
+    public : int maxScore(vector<int>&c, int k)
+    {  int ans=0 , maxi=0; 
+        for(int i=0 ; i<k;i++)
+            ans+=c[i] ;
+        maxi=ans ;
+        for(int i=k-1 ,j=c.size()-1  ; i>=0; i--,j--)
+            ans+=c[j]-c[i], maxi=max(maxi,ans);
+        return maxi;
     }
 };
