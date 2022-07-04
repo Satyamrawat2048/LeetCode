02fc8e3b8cd7668728ff36b58e8d253c9c28e0a0
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-07-04 23:52:39 +0530

    Time: 35 ms (46.56%), Space: 17.9 MB (40.68%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -3,24 +3,19 @@ public:
     int candy(vector<int>& r) {
         
         int n=r.size() ; 
-        vector<int >v(n,1),v2(n,1) ;
+        vector<int >v(n,1) ;
         
         bool chng=1;
         
-       
         for(int i=1 ; i<n ; i++)
          if (r[i]>r[i-1] ) v[i]= v[i-1]+1;
         
-        
-        for(int i=n-1 ;i>0; i--)
-        if(r[i]< r[i-1]) v2[i-1]=v2[i]+1 ;
+       int sum=v[n-1] ;
+        for(int i=n-2 ;i>=0; i--)
+       { if(r[i+1]< r[i]) v[i]= max(v[i],v[i+1]+1);
 
-        int sum= 0;
+         sum+=v[i];}
         
-        for(int i=0 ; i<n; i++)
-        {
-            sum+= max(v[i],v2[i]);
-        }
         return sum; 
     }
 };
