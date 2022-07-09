e640cad7ed7a5a98bc0511e2c1a7b7de49a9cc66
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-07-09 23:25:21 +0530

    Time: 550 ms (50.00%), Space: 85.7 MB (50.00%) - LeetHub

---
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int latestTimeCatchTheBus(vector<int>& b, vector<int>& p, int cap) {
+        int bn=b.size() ,pn=p.size() ;
+        int ans=0;
+        sort(b.begin(), b.end()) ;
+        sort(p.begin(),p.end()) ;
+     set<int> st;
+        for(auto pi:p)
+            st.insert(pi);
+
+        int j=0;
+       
+        for(int i=0 ;i<bn ; i++)
+        { int curr = b[i] ;
+             int c=0,x;
+         
+         while(j<pn && c< cap&& p[j]<=curr)
+         {
+             x= p[j] ; 
+             j++ ;
+             
+             if(st.find(x-1)==st.end()) ans =x-1 ;
+             c++ ;
+         }
+         if(c<cap){
+             while(st.find(curr)!= st.end())//starting from dept time find a time which does not exist int the set already
+              curr-- ;               
+             ans= max(ans,curr) ;
+         }
+            
+        }
+        return ans;
+    }
+};
