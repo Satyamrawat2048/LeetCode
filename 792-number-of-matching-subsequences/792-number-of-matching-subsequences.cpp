5b028d6c8b603c581fbb64f685a15a99a8be7285
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-07-20 18:38:45 +0530

    Time: 206 ms (89.37%), Space: 46.3 MB (77.99%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,32 +1,46 @@
 class Solution {
 public:
-    
-    bool issub(string w,string s)
-    {  int j=0 , k=0 ;
-
-         int wl= w.size(),sl=s.size() ;
-         while(j<wl && k< sl)
-         {
-             if(w[j]==s[k]){j++; k++; }
-         else k++ ;
+//     bool issub(string w,string s)
+//     {  int j=0 , k=0 ;
+//          int wl= w.size(),sl=s.size() ;
+//          while(j<wl && k< sl)
+//          {
+//              if(w[j]==s[k]){j++; k++; }
+//          else k++ ;
          
-          }
-        if(j==wl) return 1 ;
-     return 0;
-        
-    }
+//           }
+//         if(j==wl) return 1 ;
+//      return 0;
+//     }
     
-    int numMatchingSubseq(string s, vector<string>& w) {
-        int i=0 , c=0 ; 
+//     int numMatchingSubseq(string s, vector<string>& w) {
+//         int i=0 , c=0 ; 
+//         unordered_map<string,int> mp ;
         
-        unordered_map<string,int> mp ;
+//         for(int i=0 ; i< w.size(); i++) mp[w[i]]++ ;
         
-        for(int i=0 ; i< w.size(); i++) mp[w[i]]++ ;
+//         for(auto it: mp) 
+//             if(issub(it.first,s)) c+=it.second ;
+//         return c;
+//     }
+    int numMatchingSubseq(string& s, vector<string>& words) {
+        int ans = 0;
         
-        for(auto it: mp) 
-        {
-            if(issub(it.first,s)) c+=it.second ;
+        vector<vector<int>> mappings(26);  // stores array of indices for each character in s
+        
+        for(int i = 0; i < size(s); i++) mappings[s[i] - 'a'].push_back(i);
+        
+        for(auto& word : words) {
+            bool found = true;
+            // i = index in word | prev = index in s matched for word[i-1]
+            for(int i = 0, prev = -1; found && i < word.size(); i++) {
+                auto& v = mappings[word[i]-'a'];
+                auto it = upper_bound(begin(v), end(v), prev);   // check if current character exists in s with index > prev
+                if(it == end(v)) found = false;                  // doesn't exist
+                else prev = *it;                                 // update prev for next check
+            }
+            ans += found;
         }
-        return c;
+        return ans;
     }
 };
