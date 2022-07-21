5c846c04914f0cc4fd9fe09e8730d5007b3905ae
Satyam Rawat <78161825+Satyamrawat2048@users.noreply.github.com>
2022-07-21 23:56:16 +0530

    Time: 7 ms (21.51%), Space: 7.4 MB (93.11%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -10,19 +10,54 @@
  */
 class Solution {
 public:
-   ListNode* reverseBetween(ListNode* head, int m, int n) {
-       ListNode *dummy = new ListNode(0), *pre = dummy, *cur;
-       dummy -> next = head;
-       for (int i = 0; i < m - 1; i++) {
-           pre = pre -> next;
-       }
-       cur = pre -> next;
-       for (int i = 0; i < n - m; i++) {
-           ListNode* temp = pre -> next;
-           pre -> next = cur -> next;
-           cur -> next = cur -> next -> next;
-           pre -> next -> next = temp;
-       }
-       return dummy -> next;
-   }
+    ListNode* rev(ListNode* head,int c) {
+    
+        ListNode*prev = NULL;
+        c=c+1; 
+        while(c--){
+            ListNode*nxt = head->next;
+            head->next = prev;
+            prev = head;
+            head = nxt;
+        }
+        return prev;
+    }
+    
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(!(head->next)) return head; 
+        
+        ListNode *p1, *p2,*ptr=head ; 
+        
+          if(left==1) 
+        {   ListNode*p= head ;
+          
+              for(int i=0; i<right ;i++) p=p->next ;
+                  
+            ListNode*prev= rev(head, right-left ) ;
+           ptr->next= p ;
+         
+           return prev ;
+        }
+        
+        for(int i=0 ; i<left-2 ;i++) head=head->next ;
+        
+        p1=head ;
+        p2=head->next ;
+        
+      
+         head= head->next->next ;
+       int i=right- left ;
+        ListNode*nxt =head ,*prev=p2;
+        
+        while(i-- && head)
+        { nxt = head->next ;
+          head->next  = prev;
+         prev =head ;
+         head =nxt ;        
+        }
+        p2->next= nxt ;
+        p1->next= prev ;
+      
+        return ptr ;
+    }
 };
